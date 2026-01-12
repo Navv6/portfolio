e776de537e384acbf8c387edd202dc56783473be
Navv6 <bwme3@naver.com>
2026-01-13 02:28:53 +0900

    Simplify config: remove basePath, use basic gh-pages deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const isGithubPages = process.env.GITHUB_PAGES === 'true'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? '/pages' : '',
-  assetPrefix: isGithubPages ? '/pages/' : '',
   images: {
-    unoptimized: isGithubPages
+    unoptimized: true
   }
 }
 
